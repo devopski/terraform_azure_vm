16b36588dbe75fcbb5910ee344930db3e05423fe
devopski <dawid@devopski.com>
2021-12-18 14:11:27 +0100

    added outputs

--- main.tf
+++ main.tf
@@ -88,12 +88,3 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 }
 
-
-output "user" {
-  value = azurerm_linux_virtual_machine.example.admin_username
-}
-
-output "instance_public_ip" {
-  value = azurerm_linux_virtual_machine.example.public_ip_address
-}
-
